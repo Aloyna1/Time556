559cf82140113100ba705f49c7d6fd4f24790b80
Aloyna1 <145530923+Aloyna1@users.noreply.github.com>
2023-09-27 11:11:08 +0300

    r2
    разветвления
    
    научилась использовать If - else
    
    Сделала решение по заданию из ЛР3 по программированию

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 let x = +prompt("Введите x...");
-let y = +prompt("Введите y");
+let a = +prompt("Введите a");
+let b = +prompt("Введите b");
 
-let f = (x * Math.pow (y,2) - Math.sqrt(Math.abs(Math.pow(x,2) - 2.5 * Math.pow (10,-3) *y))) / (2 * (Math.sin(x*y)))+0.5;
+let f;
 
-console.log("d = " + f);
-
-
+if (0 < x && x <=1){
+    let mel = (x-a)/(x)
+    if (Math.sqrt(a)+x < mel){
+        mel = Math.sqrt(a)+x;
+    } 
+    if (mel > Math.sin(x)){
+        mel = Math.sin(x);
+    }
+    f = mel;
+} else if (x > 1){
+    let maxc = Math.sqrt(x)
+    if (Math.sqrt(x)< (a*x)){
+        maxc = a*x
+    }
+    f = maxc;
+} else if (x <= 0){
+    let rt = (a*x+b)
+    f = rt;
+}
+console.log(f)
